5315af313555268ecd9de8f4c121d5b4b2fbc2d1
Brendon Lavernia <lavernia@seas.upenn.edu>
2017-04-17 12:16:59 -0400

    GitRevisions working with tests

--- GitRevisions.hs
+++ GitRevisions.hs
@@ -1,12 +1,19 @@
 {-# OPTIONS -fwarn-tabs -fwarn-incomplete-patterns  #-}
 
-module GitRevisions (revParseTree, CaretQualifier(Head,Exclamation,Index, 
-                      Dash), RevArg(RevId,Exclude,XOR,Caret)) where 
+module GitRevisions (revParseTree, Relation(..), CaretQualifier(..), RevArg(..)
+                    ) where 
 
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-data CaretQualifier = Head | Exclamation | Index Int | Dash Int
+data CaretQualifier = Head | Exclamation | Dash Word
+
+data Relation a = Ancestor a | Parent a
+
+{-
+ - DirectParents = A^2 = C
+ - Ancestors =     A~2 = D
+ -}
 
 {-
  - RevId = A
@@ -18,55 +25,83 @@ data CaretQualifier = Head | Exclamation | Index Int | Dash Int
  - Caret Dash x = A^-x
  - XOR (Exclude A) B = A..B 
  -}
-data RevArg a = RevId a | 
+data RevArg a  = RevId a | 
                  Exclude (RevArg a) | 
                  XOR (RevArg a) (RevArg a) | 
-                 Caret a CaretQualifier
+                 Caret a CaretQualifier |
+                 Ancestry a [Relation Word]
 
 -- | Returns all the ancestors of Rev x along with x
 -- | !!! Make sure to not add parents multiple times
-getAllAncestors :: (Ord a, Show a) => a -> (a -> Set a) -> Set a
-getAllAncestors x getParents = case Set.toList $ getParents x of
-                      [] -> Set.empty
-                      -- Change to Set.unions
-                      xs -> foldr (\y acc -> getAllAncestors y getParents `Set.union` acc) 
-                              (Set.singleton x) xs 
+getAllAncestors :: (Ord a, Show a) => a -> (a -> Set a) -> Set a -> Set a
+getAllAncestors x f s =
+  if Set.member x s
+  then s
+  else do
+      let newSet = Set.insert x s
+      case Set.toList $ f x of
+         [] -> newSet
+         -- Change to Set.unions
+         xs -> foldr (\y acc -> getAllAncestors y f acc `Set.union` acc) 
+                      newSet xs 
 {- 
  - Takes a list of Rev arguments and returns the matching set of Revs
  -}
-revParseTree :: (Ord a, Show a) => [RevArg a] -> (a -> Set a) -> Set a
+revParseTree :: (Ord a, Show a) => [RevArg a ] -> (a -> Set a) -> Set a
 revParseTree [] _  = Set.empty
-revParseTree (RevId x:xs) getParents = getAllAncestors x getParents 
+
+revParseTree (RevId x:xs) f = getAllAncestors x f Set.empty 
                                        `Set.union` 
-                                       revParseTree xs getParents
-revParseTree (Exclude x:xs) getParents = revParseTree xs getParents 
-                                         `Set.difference` 
-                                         revParseTree [x] getParents
-revParseTree (XOR (Exclude rev1) rev2:xs) getParents = 
-  revParseTree (Exclude rev1 : rev2 : xs) getParents
-revParseTree (XOR rev1 rev2:xs) getParents =
-  let set1 = revParseTree [rev1] getParents 
-      set2 = revParseTree [rev2] getParents 
+                                       revParseTree xs f
+
+revParseTree (XOR (Exclude rev1) rev2:xs) f = 
+  revParseTree (Exclude rev1 : rev2 : xs) f
+
+revParseTree (Exclude x:xs) f = revParseTree xs f 
+                                `Set.difference`
+                                case Set.toList (revParseTree [x] f) of
+                                  [] -> Set.empty
+                                  y:_ -> getAllAncestors y f Set.empty 
+
+revParseTree (XOR rev1 rev2:xs) f =
+  let set1 = revParseTree [rev1] f 
+      set2 = revParseTree [rev2] f 
   in (set1 `Set.union` set2) 
      `Set.difference` 
      (set1 `Set.intersection` set2) 
      `Set.union` 
-     revParseTree xs getParents 
-revParseTree (Caret x Head:xs) getParents = 
-  getAllAncestors x getParents 
+     revParseTree xs f 
+
+revParseTree (Caret x Head:xs) f = 
+  getAllAncestors x f Set.empty 
   `Set.difference` 
   Set.singleton x 
   `Set.union` 
-  revParseTree xs getParents 
-revParseTree (Caret x Exclamation:xs) getParents = 
-  let ys = foldl (\acc y -> Exclude (RevId y) : acc) 
-            [RevId x] (Set.toList (getParents x))
-  in revParseTree (ys ++ xs) getParents 
-revParseTree (Caret x (Index idx):xs) getParents = 
-  Set.singleton (Set.toList (getParents x) !! idx) 
-  `Set.union` 
-  revParseTree xs getParents 
-revParseTree (Caret x (Dash idx):xs) getParents = 
-  revParseTree (XOR (Exclude (Caret x (Index idx))) (RevId x) : xs) getParents 
+  revParseTree xs f 
 
+revParseTree (Caret x Exclamation:xs) f = 
+  let ys = foldl (\acc y -> Exclude (RevId y) : acc) 
+            [RevId x] (Set.toList (f x))
+  in revParseTree (ys ++ xs) f 
+
+revParseTree (Caret x (Dash idx):xs) f = 
+  revParseTree (XOR (Exclude (Ancestry x [Parent idx])) (RevId x) : xs) f 
+
+revParseTree (Ancestry x as:xs) f = 
+  case traverseFamily x as f of
+    Nothing -> Set.empty
+    Just y -> Set.insert y (revParseTree xs f)
+
+traverseFamily :: (Ord a, Show a) => 
+                  a -> [Relation Word] -> (a -> Set a) -> Maybe a
+traverseFamily x [] _ = Just x 
+traverseFamily x (Ancestor i:as) f =
+  if i <= 0
+  then traverseFamily x as f
+  else 
+    case Set.toList (f x) of
+      [] -> Nothing
+      p:ps -> traverseFamily p (Ancestor (i-1):as) f
+traverseFamily x (Parent i:as) f =
+  traverseFamily ((x : Set.toList (f x)) !! fromIntegral i) as f
 
